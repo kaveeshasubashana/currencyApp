b936eb72ba2b590618904f0e3f058c14c898a6d2
Kaveesha Subashana <kaveeshasubashana@gmail.com>
2025-05-20 23:29:32 +0530

    tailwindcss

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function MainPage() {
   return (
-    <div>MainPage</div>
-  )
+    <div>
+         <h1>Convert your currancies</h1>
+         <p>A universal currency converter is an app or web tool that allows for the quick
+             conversion of any currency into any other currency. Universal converters typically
+              use the most recent market prices in the foreign exchange market.</p>
+
+    </div>
+   
+  );
 }
